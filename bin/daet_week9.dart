be2b6896e56d60ad92b3e0868c5b06b3a810ae31
Khunaiko <pasuaiko21089@gmail.com>
2023-07-20 09:10:04 +0700

    first commit

---
+++ bin/daet_week9.dart
@@ -0,0 +1,5 @@
+import 'package:daet_week9/daet_week9.dart' as daet_week9;
+
+void main(List<String> arguments) {
+  print('Hello world: ${daet_week9.calculate()}!');
+}
---
+++ test/daet_week9_test.dart
@@ -0,0 +1,8 @@
+import 'package:daet_week9/daet_week9.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
